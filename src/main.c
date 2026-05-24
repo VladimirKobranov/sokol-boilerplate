ed6a68e62a4877af4c47285745a763cfff3050b9
VladimirKobranov <josephclturok@gmail.com>
2026-05-25 01:33:53 +0900

    allias

--- src/main.c
+++ src/main.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 // sokol
 #include <sokol_app.h>
 #include <sokol_gfx.h>
@@ -8,18 +7,24 @@
 #include "cgltf.h"
 // matrix
 #include <HandmadeMath.h>
+#define mat4 HMM_Mat4
 // shader
 #include <shader_glsl.h>
 
 static struct {
+  // sokol
   sg_pipeline pip;
   sg_bindings bind;
   sg_pass_action pass_action;
   sg_buffer vbuf;
   sg_buffer ibuf;
-  int num_indices;
   sg_index_type index_type;
 } state;
+
+static struct {
+  // glb\gltf model
+  int num_indices;
+} model;
 
 void loadFile() {
   cgltf_options options = {0};
@@ -45,7 +50,7 @@ void loadFile() {
   unsigned char *ibase = (unsigned char *)idx->buffer_view->buffer->data;
   void *idx_data = ibase + idx->buffer_view->offset + idx->offset;
   cgltf_bool is_uint16 = idx->component_type == cgltf_component_type_r_16u;
-  state.num_indices = (int)idx->count;
+  model.num_indices = (int)idx->count;
   state.index_type = is_uint16 ? SG_INDEXTYPE_UINT16 : SG_INDEXTYPE_UINT32;
   state.ibuf = sg_make_buffer(&(sg_buffer_desc){
       .usage = {.index_buffer = true},
@@ -62,15 +67,14 @@ void init() {
   loadFile();
 
   sg_shader shd = sg_make_shader(triangle_shader_desc(sg_query_backend()));
-  state.pip = sg_make_pipeline(
-      &(sg_pipeline_desc){.shader = shd,
-                          .layout = {.attrs = {[ATTR_triangle_position].format =
-                                                   SG_VERTEXFORMAT_FLOAT3}},
-                          .index_type = state.index_type,
-                          .face_winding = SG_FACEWINDING_CCW,
-                          .depth = {.compare = SG_COMPAREFUNC_LESS_EQUAL,
-                                    .write_enabled = true},
-                          .label = "cube-pipeline"});
+  state.pip = sg_make_pipeline(&(sg_pipeline_desc){
+      .shader = shd,
+      .layout = {.attrs = {[ATTR_triangle_position].format =
+                               SG_VERTEXFORMAT_FLOAT3}},
+      .index_type = state.index_type,
+      .face_winding = SG_FACEWINDING_CCW,
+      .depth = {.compare = SG_COMPAREFUNC_LESS_EQUAL, .write_enabled = true},
+      .label = "cube-pipeline"});
 
   state.pass_action = (sg_pass_action){
       .colors[0] = {.load_action = SG_LOADACTION_CLEAR,
@@ -83,14 +87,12 @@ void frame() {
   float angle = (float)sapp_frame_count() * 0.01f;
   float aspect = (float)sapp_width() / (float)sapp_height();
 
-  HMM_Mat4 model = HMM_Rotate_RH(angle, HMM_V3(0.25, 0.5, 0));
-  HMM_Mat4 view =
-      HMM_LookAt_RH(HMM_V3(7, 0, 0), HMM_V3(0, 0, 0), HMM_V3(0, 1, 0));
+  mat4 model_matrix = HMM_Rotate_RH(angle, HMM_V3(0.25, 0.5, 0));
+  mat4 view = HMM_LookAt_RH(HMM_V3(7, 0, 0), HMM_V3(0, 0, 0), HMM_V3(0, 1, 0));
   // ZO = Zero to One; clipping
-  HMM_Mat4 proj =
-      HMM_Perspective_RH_ZO(HMM_AngleDeg(50.0f), aspect, 0.1f, 100.0f);
-  HMM_Mat4 mv = HMM_MulM4(view, model);
-  HMM_Mat4 mvp = HMM_MulM4(proj, mv);
+  mat4 proj = HMM_Perspective_RH_ZO(HMM_AngleDeg(50.0f), aspect, 0.1f, 100.0f);
+  mat4 mv = HMM_MulM4(view, model_matrix);
+  mat4 mvp = HMM_MulM4(proj, mv);
   vs_params_t vs_params = {.mvp = mvp, .mv = mv};
 
   sg_begin_pass(
@@ -99,7 +101,7 @@ void frame() {
   sg_apply_bindings(&state.bind);
   sg_range uniform_range = {.ptr = &vs_params, .size = sizeof(vs_params)};
   sg_apply_uniforms(UB_vs_params, &uniform_range);
-  sg_draw(0, state.num_indices, 1);
+  sg_draw(0, model.num_indices, 1);
   sg_end_pass();
   sg_commit();
 }
